84123a3133400b546c48f8534b62ba3fa1288986
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2017-07-06 08:24:30 -0300

    Update l1q27.py

--- l1q27.py
+++ l1q27.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Computador:
 
 	def __init__(self, pmRef, pmNome, anoCompra, sO):
@@ -7,4 +9,15 @@ class Computador:
 		self.sistemaOperacional = sO
 
 	def __str__(self):
-		result = 'Placa Mae: ' + str()
+		result = 'Placa Mae Referencia: ' + str(self.placaMaeRef) + '\n'
+		result += 'Placa Mae Nome: ' + str(self.placaMaeNome) + '\n'
+		result += 'Ano Compra: ' + str(self.anoCompra) + '\n'
+		result += 'Sistema Operacional: ' + str(self.sistemaOperacional) 
+		return result
+
+
+	def obter_configuracao(self):
+		return str(self.placaMaeRef) + ' - ' + str(self.placaMaeNome)
+
+	def obter_tempo_de_uso(self):
+		return date.year - self.anoCompra
