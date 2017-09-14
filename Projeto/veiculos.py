88f80dc2bfe046cae2c3e0c348458efb9ed4c70a
lucasebs <lucasebatista@gmail.com>
2017-09-13 21:11:12 -0300

    inicio 13-09

--- Projeto/veiculos.py
+++ Projeto/veiculos.py
@@ -20,3 +20,17 @@ class Veiculos(object):
 		while temp is not None:
 			print(Veiculo.info_veiculo)
 			temp = temp.proximo_veiculo
+
+	def remover_especifico(self, chassi)
+		if self.esta_vazia():
+			return
+
+		vclo_anterior = self.referencia
+		vclo_corrente = vclo_anterior.proximo_veiculo
+
+		while vclo_corrente is not None and vc:
+
+			vclo_anterior = vclo_corrente
+			vclo_corrente = vclo_corrente.proximo_veiculo
+
+		if 
