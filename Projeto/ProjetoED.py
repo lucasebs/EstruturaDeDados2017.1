88d3441634fb6351c4f69257a9ab27b222b56c84
lucasebs <lucasebatista@gmail.com>
2017-09-14 00:02:58 -0300

    SO check. Clear Screen

--- Projeto/ProjetoED.py
+++ Projeto/ProjetoED.py
@@ -1,5 +1,7 @@
 from funcoes.bib import *
 from adt.concessionaria import Concessionaria
+from sys import platform
+import os
 
 
 c1 = Concessionaria()
@@ -7,10 +9,10 @@ c1 = Concessionaria()
 while True: 
 	print(menu())
 	seleciona_menu(captura_menu(), c1)
-	parada = int(input())
-	if (parada == -1):
+	parada = input()	
+	if (parada.lower() == 'sair'):
 		break
-
-
-
-
+	if platform == "linux" or platform == "linux2":
+		os.system('clear')
+	elif platform == "win32":
+		os.system('cls')
