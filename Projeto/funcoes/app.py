355614ad04a6782c88d7234c1fc53fa71254d118
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2017-09-22 18:05:35 -0300

    DocString app.py
    seleciona_menu(), busca_veiculo() e adiciona_veiculo()

--- Projeto/funcoes/app.py
+++ Projeto/funcoes/app.py
@@ -21,30 +21,88 @@ def captura_menu():
 	return op
 
 def seleciona_menu(op, c):
+	'''
+	Function: 
+		seleciona_menu : Recebe como parametros a operacao do menu
+			e seleciona funcao respectiva a operacao
+	Attributes: 
+		@param (op) (int) : Identificacao da operacao.
+			1 -	Buscar Veiculo
+			2 -	Adicionar Veiculo
+			3 -	Remover Veiculo
+		@param (c) (obj) : Objeto do tipo concessionaria
+	Returns: 
+		True (bool) : Retorna True em caso de opcao para sair do sistema
+	'''
+	# Verifica operacao recebida como parametro e executa a respectiva funcao
+	# ou se usuário deseja sair do sistema
+	# 1 - Buscar Veiculo
 	if (op == '1'):
+		# Executa funcao de busca passando a concessionaria c como parametro
 		busca_veiculo(c)
+	# 2 - Adicionar Veiculo
 	elif (op == '2'):
+		# Executa funcao de adicao passando a concessionaria c como parametro
 		adiciona_veiculo(c)
+	# 3 - Remover Veiculo
 	elif (op == '3'):
+		# Executa funcao de remocao passando a concessionaria c como parametro
 		remove_veiculo(c)
+	# Se receber da entrada padrao a str "sair", retorna True
 	elif op.lower() == 'sair':
 		return True
+	# Se for recebida qualquer outra informacao na entrada padrao imprime erro da opcao
 	else:
 		print("Opção Inválida!")
 
 def busca_veiculo(c):
+	'''
+	Function: 
+		busca_veiculo : Recebe como parametro a concessionaria e executa
+			na lista de veiculos desta a buscar do veiculo
+	Summary: 
+		chassi (int) : Recebe da funcao captura_entrada() um numero de chassi
+		veiculo (obj) : Recebe um objeto do tipo Veiculo da funcao buscar()
+	Attributes: 
+		@param (c) (obj) : Objeto do tipo concessionaria
+	'''
+	# Exibe cabecalho da operacao de busca 'b'
 	cabecalho('b')
+	# Recebe numero de chassi do retorno da funcao captura_entrada()
 	chassi = captura_entrada(c, 'b')
+	# Recebe um objeto do tipo Veiculo da funcao buscar() da lista de veiculos
+	# da concessionaria c
 	veiculo = c.veiculos.buscar(chassi)
+	# Imprime o veiculo
 	imprime_veiculo(veiculo)
 
 def adiciona_veiculo(c):
+	'''
+	Function: 
+		adiciona_veiculo : Recebe como parametro a concessionaria e executa
+			a adicao de um veiculo na lista de veiculos da concessionaria
+	Summary: 
+		iv (tupla) : Recebe da funcao captura_entrada() uma tupla	
+	Attributes: 
+		@param (c) (obj) : Objeto do tipo concessionaria
+	'''
+	# Exibe cabecalho da operacao de busca 'b'
 	cabecalho('a')
+	# iv = Informacoes do Veiculo. Recebe tupla da funcao captura_entrada() com 
+	# as informacoes do veiculo para ser armazenado na lista de veiculos da 
+	# concessionaria c
 	iv = captura_entrada(c, 'a')
+	# Verifica se iv nao e um valor booleano True, sendo assim uma tupla
 	if iv != True: 
+		# Adiciona o veiculo, com as informacoes do mesmo contidas em iv
+		# na lista de veiculos da concessionaria
 		c.veiculos.adicionar(int(iv[0]), iv[1], int(iv[2]), iv[3], float(iv[4]))
+		# Incrementa 1 na quantidade de veiculos apos adicionar o veiculo na lista
 		c.quantidade_veiculos += 1
+		# Executa funcao atualiza_arquivo() passando como paramentro a 
+		# concessionaria c
 		atualiza_arquivo(c)
+		# Executa funcao sucesso() para a operacao de adicao de veiculo
 		sucesso('a')
 	
 
@@ -81,7 +139,6 @@ def verifica_fim():
 def carrega_lista(c):
 	arq = open('arquivos/veiculos.txt', 'r')
 	linhas = arq.readlines()
-	result = ''
 	
 	for linha in linhas:
 		if "Chassi do carro: " in linha:
@@ -94,7 +151,6 @@ def carrega_lista(c):
 			marca = linha[7: -1]
 		elif "Preço: R$ " in linha:
 			preco = linha[10: -1]
-			print(chassi,nome, ano, marca, preco)
 			c.veiculos.adicionar(int(chassi),nome, int(ano), marca, float(preco))
 	
 	arq.close()
