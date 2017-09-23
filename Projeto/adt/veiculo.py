64b160638b8b62b027cc3a6738ef23d2a5043a0f
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2017-09-22 23:50:35 -0300

    veiculo DocString finalizada

--- Projeto/adt/veiculo.py
+++ Projeto/adt/veiculo.py
@@ -1,5 +1,15 @@
 class Veiculo():
-	"""docstring for Veiculo"""
+	"""
+	Class:
+		Veiculo : Objeto veiculo
+	Summary:
+		chassi 	(int) 	: Numero de chassi do veiculo.
+		nome 	(str)	: Nome do veiculo correspondente ao chassi 
+			informado
+		ano 	(int)	: Ano de fabricacao do veiculo
+		marca 	(str)	: Marca do veiculo 
+		preco 	(float)	: Preco em Real do veiculo
+	"""
 	def __init__(self,chassi, nome, ano, marca, preco):
 		self.chassi = chassi
 		self.nome = nome
