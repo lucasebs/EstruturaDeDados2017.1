d3af5a073f3244655d596aa3e7224a543320d546
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2017-09-22 23:40:58 -0300

    concessionaria DocString finalizada

--- Projeto/adt/concessionaria.py
+++ Projeto/adt/concessionaria.py
@@ -2,7 +2,15 @@ from .veiculos import Veiculos
 from funcoes.bib import *
 
 class Concessionaria():
-	"""docstring for Concessionaria"""
+	"""
+	Class: 
+		Concessionaria 
+	Summary:
+		quantidade_veiculos (int) : Quantidade de veiculos registrados
+			na concessionaria
+		veiculos (list) : Lista do tipo Veiculos contendo veiculos
+			registrados na concessionaria
+	"""
 	def __init__(self):
 		self.quantidade_veiculos = 0
 		self.veiculos = Veiculos()
