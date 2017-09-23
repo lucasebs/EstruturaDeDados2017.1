e03351d6f199b068354e36035c3ab77cbe1c8de4
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2017-09-22 23:38:07 -0300

    app DocString finalizada

--- Projeto/funcoes/app.py
+++ Projeto/funcoes/app.py
@@ -80,13 +80,13 @@ def adiciona_veiculo(c):
 	'''
 	Function: 
 		adiciona_veiculo : Recebe como parametro a concessionaria e executa
-			a adicao de um veiculo na lista de veiculos da concessionaria
+			a adicao de um veiculo na lista de veiculos da concessionaria c
 	Summary: 
 		iv (tupla) : Recebe da funcao captura_entrada() uma tupla	
 	Attributes: 
 		@param (c) (obj) : Objeto do tipo concessionaria
 	'''
-	# Exibe cabecalho da operacao de busca 'b'
+	# Exibe cabecalho da operacao de busca 'a'
 	cabecalho('a')
 	# iv = Informacoes do Veiculo. Recebe tupla da funcao captura_entrada() com 
 	# as informacoes do veiculo para ser armazenado na lista de veiculos da 
@@ -107,40 +107,117 @@ def adiciona_veiculo(c):
 	
 
 def remove_veiculo(c):
+	'''
+	Function: 
+		remove_veiculo : Recebe como parametro a concessionaria e executa
+			a remocao de um veiculo na lista de veiculos da concessionaria c
+	Summary: 
+		chassi (int) : Recebe da funcao captura_entrada() um numero de chassi
+		removido (str/bool) : Receve da funcao remover() da lista de veiculos da 
+		concessionaria um valor booleano ou str com motivo da nao remocao
+	Attributes: 
+		@param (c) (obj) : Objeto do tipo concessionaria
+	'''
+	# Exibe cabecalho da operacao de busca 'b'
 	cabecalho('r')
+	# Recebe numero de chassi do retorno da funcao captura_entrada()
 	chassi = captura_entrada(c, 'r')
+	# Recebe valor booleano ou str da funcao remover() da lista de veiculos da concessionaria
 	removido = c.veiculos.remover(chassi)
+	# Verifica se removido tem valor True, significando que o veiculo foi removido da lista
+	# de veiculos da concessionaria. 
 	if removido == True:
+		# Decrementa 1 na quantidade de veiculos apos adicionar o veiculo na lista
 		c.quantidade_veiculos -= 1
+		# Executa funcao atualiza_arquivo() passando como paramentro a 
+		# concessionaria c
 		atualiza_arquivo(c)
+		# Executa funcao sucesso() para a operacao de remocao de veiculo
 		sucesso('r')
+	# Se veiculo nao tiver sido removido exibe o motivo na tela
 	else:
 		print(removido)
 
 def atualiza_arquivo(c):	
+	'''
+	Function: 
+		atualiza_arquivo : Recebe como parametro a concessionaria e executa
+			a atualizacao do arquivo de registro(veiculos.txt)
+	Summary: 
+		arq (file) : Recebe arquivo aberto habilitado para escrita
+	Attributes: 
+		@param (c) (obj) : Objeto do tipo concessionaria
+	'''
+	# Abre arquivo veiculos.txt no modo de escrita e define nome como arq
 	arq = open('arquivos/veiculos.txt', 'w')	
+	# Verifica se a lista de veiculos esta vazia
+	# Se tiver valor diferente de None escreve lista de veiculos no arquivo
 	if c.veiculos.listar() is not None:
 		arq.write(c.veiculos.listar())
+	# Se nao, escreve str vazia no arquivo
 	else:
 		arq.write('')
+	# Fecha o aquivo veiculos.txt definido como arq
 	arq.close()
 
 def limpar_tela():
+	'''
+	Function: 
+		limpar_tela : Limpa a tela do terminal de operacao do sistema
+	'''
+	# Verifica o SO e executa o comando respectivo, se Linux ou Windows
 	if platform == "linux" or platform == "linux2":
 		os.system('clear')
 	elif platform == "win32":
 		os.system('cls')
 
 def verifica_fim():
+	'''
+	Function: 
+		verifica_fim : Recebe um valor da entrada e retorna se o sistema
+			deve ser encerrado
+	Summary: 
+		parada (str) : Recebe da entrada padrao uma str 
+	Returns: 
+		True (bool) : Retorna True em caso de opcao para sair do sistema
+	'''
+	# Recebe da entrada padrao uma str de qualquer valor
 	parada = input()	
+	# Verifica se str transformada em minuscula e igual a 'sair', caso sim
+	# retorna True para encerramento do sistema
 	if (parada.lower() == 'sair'):
 		return True
 
 def carrega_lista(c):
+	'''
+	Function: 
+		carrega_lista : Recebe como parametro a concessionaria e executa
+			a leitura do arquivo de lista de veiculos(veiculos.txt)
+			passando assim os dados para a lista interna do programa
+	Summary: 
+		arq (file) : Recebe arquivo aberto habilitado para escrita
+		linhas (str) : Recebe da funcao readlines() do arquivo veiculos.txt
+			uma str
+		chassi 	(int) 	: Numero de chassi do veiculo.
+		nome 	(str)	: Nome do veiculo correspondente ao chassi 
+			informado
+		ano 	(int)	: Ano de fabricacao do veiculo
+		marca 	(str)	: Marca do veiculo 
+		preco 	(float)	: Preco em Real do veiculo
+	Attributes: 
+		@param (c) (obj) : Objeto do tipo concessionaria
+	'''
+	# Abre arquivo veiculos.txt no modo de leitura e define nome como arq
 	arq = open('arquivos/veiculos.txt', 'r')
+	# Recebe da funcao readlines() do arquivo veiculos.txt uma str contendo
+	# as linhas do arquivo 
 	linhas = arq.readlines()
 	
+	# Laço para a leitura de cada linha das linhas do aquivo individualmente
 	for linha in linhas:
+		# Verifica conteudo da linha lida. E assum que conteudo restante da
+		# linha, além da descricao do dado é o proprio dados, definindo assim
+		# eles em suas respectivas variaveis
 		if "Chassi do carro: " in linha:
 			chassi = linha[17: -1]
 		elif "Nome do carro: " in linha:
@@ -151,6 +228,9 @@ def carrega_lista(c):
 			marca = linha[7: -1]
 		elif "Preço: R$ " in linha:
 			preco = linha[10: -1]
+			# Adiciona o veiculo na lista de veiculos da concessionaria
+			# com as informacoes do mesmo contidas nas variaveis definidas
+			# a partir da leitura das linhas do arquivo
 			c.veiculos.adicionar(int(chassi),nome, int(ano), marca, float(preco))
-	
+	# Fecha o aquivo veiculos.txt definido como arq
 	arq.close()
