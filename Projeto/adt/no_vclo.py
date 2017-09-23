ea7578c00ca610c9caca3d79394fdaf251fde593
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2017-09-22 23:48:50 -0300

    no_vclo DocString finalizada

--- Projeto/adt/no_vclo.py
+++ Projeto/adt/no_vclo.py
@@ -1,5 +1,11 @@
 class No_vclo:
-	"""docstring for No_vclo"""
+	"""
+	Class: 
+		No_vclo : No da lista de veiculos
+	Summary:
+		info_veiculo (str) : Recebe str com dados do veiculo. Inicialmente None.
+		proximo_veiculo (obj) : Objeto do tipo No_vclo
+	"""
 	def __init__(self):
 		self.info_veiculo = None
 		self.proximo_veiculo = None
